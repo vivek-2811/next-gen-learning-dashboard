75867b223e43f609671e673bf8e8e2f3b3adc5c4
vivek-2811 <vivekavasthi6548@gmail.com>
2026-06-03 23:36:54 +0530

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black text-white flex items-center justify-center">
+      Dashboard Coming Soon
+    </main>
+  );
+}
